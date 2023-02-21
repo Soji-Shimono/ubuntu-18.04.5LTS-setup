906152799f0f9e3375892f4bd3b6382d87e30a1a
Soji-Shimono <Shimon0125@yahoo.co.jp>
2023-02-22 01:26:22 +0900

    Add files via upload
    Add setup scripts

---
+++ setup1.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#package update
+sudo apt update
+sudo apt upgreade
+#sudo systemctl didsable ----
+
+#network setting
+cd /etc/netplan
+sudo rm  50-cloud-init.yaml
+sudo wget https://raw.githubusercontent.com/Soji-Shimono/ubuntu-18.04.5LTS-setup/main/50-cloud-init.yaml
+sudo netplan apply
+
+#can setting
+cd /boot/firmware
+sudo rm usercfg.txt
+sudo wget https://raw.githubusercontent.com/Soji-Shimono/ubuntu-18.04.5LTS-setup/main/usercfg.txt
+
+sudo apt install can-utils
+sudo ip link set can0 type can0 bitrate 1000000
+sudo ip link set can0 up
